6259e80978856eba86514d38c424190153e1da59
devMilen <20stilen09@gmail.com>
2025-07-23 16:33:56 +0300

    window

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <print>
 #include <iostream>
-#include <array>
-
-int randomFunc() {
-	std::cout << "yes he  he  he  he";
-	return  1;
-}
+#include "glfw3.h"
 
 int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "", nullptr, nullptr);
+	while (!glfwWindowShouldClose(window))
+		glfwPollEvents();
 	std::print("is done repository: {} he he he", true);
 	std::cin.get();
 }
